152a490a83434d34c1959a28c1770ba236cde0ed
ARYAN_6582896578 <aryanmovalia@proton.me>
2025-02-09 10:24:22 +0530

    hm

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <>
+     <p className='font-mono text-white bg-gray-800 '>hi</p>
+    </>
+  )
+}
+
+export default App
