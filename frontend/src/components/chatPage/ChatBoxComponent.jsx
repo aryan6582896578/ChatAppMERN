b81d1fb2f8ab9150e7df1fa3a669fb523454a0fa
ARYAN_6582896578 <aryanmovalia@proton.me>
2025-06-01 02:04:14 +0530

    scroll down button scroll old message scroll to bottom when enter

--- frontend/src/components/chatPage/ChatBoxComponent.jsx
+++ frontend/src/components/chatPage/ChatBoxComponent.jsx
@@ -10,7 +10,7 @@ export function ChatBoxComponent() {
   let date = new Date();
 
   const [messageData, setmessageData] = useState("");
-
+  const[uId,suId]=useState("")
   
   const [displayMessageSocket,setdisplayMessageSocket]= useState([]);
   const [displayMessageDb,setdisplayMessageDb]= useState([]);
@@ -20,18 +20,44 @@ export function ChatBoxComponent() {
 
   const [topReached,settopReached]=useState(false)
   const chatDiv = useRef(null)
+  const chatDivBottom = useRef(null)
+  const chatDivTop = useRef(null)
 
+  const[scrollDown,setscrollDown]=useState(true);
   function scrollBottom(){
     if (chatDiv.current) {
       chatDiv.current.scrollTop = chatDiv.current.scrollHeight;
     }
   }
   useEffect(() => {
-    scrollBottom()
+    if(displayMessageDb.length<=20){
+      scrollBottom()
+    }
   }, [displayMessageDb]);
-  
 
+  useEffect(() => {
+    if (displayMessageSocket?.length && displayMessageSocket[displayMessageSocket.length - 1].userId === uId) {
+      chatDivBottom.current?.scrollIntoView();
+    }
+  }, [displayMessageSocket]);
 
+  function scrollTopPage(){
+
+    if(topReached && getMessageCount.current<getMaxMessageCount.current){
+       if(topReached && getMessageCount.current<getMaxMessageCount.current){
+       if(chatDivTop.current.scrollHeight===0){
+        chatDiv.current?.scroll({
+          top: 10,
+        })
+        getMessageCount.current += 20;
+         getMessage()  
+         settopReached(false)
+       }
+  }
+}}
+   useEffect(() => {
+    scrollTopPage()
+  }, [topReached])
   async function getMessage() {
       const messageData = await axios.get(`${import.meta.env.VITE_SERVERURL}${import.meta.env.VITE_VERSION}/messageData/${parms.serverId}/${parms.channelId}/${getMessageCount.current}`,{
         withCredentials: true,
@@ -67,9 +93,20 @@ export function ChatBoxComponent() {
       const channelId = parms.channelId
       socket.auth = {jwtToken,serverId,channelId}
   }
+  
+  async function getUserData() {
+    const userData = await axios.get(`${import.meta.env.VITE_SERVERURL}${import.meta.env.VITE_VERSION}/verify`,{
+        withCredentials: true,
+      });
+    
+    const userId = userData.data.userId;
+    suId(userData.data.userId)
+    return userId;
+  }
   useEffect(() => {
+    getUserData()
     getMessage()
-
+    
     setSocketData()
     socket.connect()
     socket.on(`${parms.serverId}/${parms.channelId}`,async (messageData)=>{
@@ -83,11 +120,15 @@ export function ChatBoxComponent() {
     }
   }, [parms.serverId,parms.channelId])
 
- 
+
   return (
     <div className="bg-cyan-400 w-[100%] h-[100%]" >
       <div className="bg-black flex flex-col h-[100%]">
-        <div className="bg-primaryColor flex h-[100%] flex-col overflow-y-auto" ref={chatDiv}>
+        <div className="bg-primaryColor flex h-[100%] flex-col overflow-y-auto" ref={chatDiv} onScroll={(e)=>{if(e.target.scrollTop===0){
+            settopReached(true) 
+          }            
+        }}>
+          <div ref={chatDivTop}></div>
           <div>
             {displayMessageDb?.map((data,x)=>{
               return( 
@@ -108,14 +149,17 @@ export function ChatBoxComponent() {
                 </div>
               )
             })} 
+            <div ref={chatDivBottom}></div>
           </div>   
           
         </div>
         
-        <div className="min-h-[55px] flex bg-primaryColor">
+        <div className="min-h-[55px] flex bg-primaryColor relative">
+          
               <div contentEditable className="w-[100%] m-[5px] mb-[0xp] ml-[10px] outline-none bg-otherColor bg-opacity-[4%] text-[20px] pl-[10px] pt-[5px] rounded-[5px] text-otherColor text-opacity-[70%]"  onKeyDown={sendMessage} spellCheck={true}  onInput={(e) => {
                     setmessageData(e.target.innerText);         
               }}/>
+            {scrollDown?<button className=" bg-purple-500 h-[60%] mt-auto mb-auto w-[5px] rounded-[10px]" onClick={scrollBottom}/>:""}  
         </div>
       
       </div>
